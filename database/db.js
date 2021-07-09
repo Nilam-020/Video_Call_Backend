8cf1b4a2c87bb712d83205dd076c6575c414a551
ankit016 <alyohinadhikari@gmail.com>
2021-07-09 09:29:06 +0545

    cloud database added

--- database/db.js
+++ database/db.js
@@ -1,12 +1,14 @@
 const mongoose = require('mongoose');
 const colors = require("colors");
-const mongoPath = 'mongodb://127.0.0.1:27017/appointmentBooking_Api'
+// const mongoPath = 'mongodb://127.0.0.1:27017/appointmentBooking_Api'
+//
+const mongoPath='mongodb+srv://VideoCallApplication:ztdxVWpDt0oYvUVI@videocallapp.4llyw.mongodb.net/videoCallApplication_Api?retryWrites=true&w=majority'
 mongoose.connect(mongoPath, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
     useFindAndModify:false
-});
+})
 console.log(
     `MongoDB connected`.blue.bold
 );
