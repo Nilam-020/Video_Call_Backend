4887f0deaef6732a31bf60aea13ae4a04489077b
ankit016 <alyohinadhikari@gmail.com>
2021-08-11 09:57:09 +0545

    packaged added

--- app.js
+++ app.js
@@ -3,22 +3,26 @@ const dotenv = require("dotenv");
 const morgan = require("morgan");
 const bodyParser = require('body-parser')
 const app = express();
+
 app.use(bodyParser.urlencoded({ extended: false }));
 const connectDB = require('./database/db')
 const colors = require("colors");
 const cors = require('cors')
-const path = require('path')
 
 const userRoute = require('./routes/userRoute');
 const appointmentRoute = require('./routes/appointmentRoute');
 const doctorRoute = require('./routes/DoctorRoute');
-// const prescriptonRouter=require('./routes/prescriptionRoute');
-
-
-// const instanceRouter = require('./routes/appointmentInstanceRoute');
 const { mongo } = require('mongoose');
 app.use(express.json());
-app.use("/images", express.static(path.join(__dirname, "/images")))
+const server = require("http").createServer(app);
+
+const io = require("socket.io")(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
+
 // Connect to mongoDB database
 app.use(cors());
 app.use(userRoute);
@@ -29,11 +33,27 @@ dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send('server is connected')
 })
 
-app.listen(
+io.on("connection", (socket) => {
+    socket.emit("me", socket.id);
+
+    socket.on("disconnect", () => {
+        socket.broadcast.emit("callEnded")
+    });
+
+    socket.on("callUser", ({ userToCall, signalData, from, name }) => {
+        io.to(userToCall).emit("callUser", { signal: signalData, from, name });
+    });
+
+    socket.on("answerCall", (data) => {
+        io.to(data.to).emit("callAccepted", data.signal)
+    });
+});
+
+server.listen(
     PORT,
     console.log(
         `Server running in mode : ${process.env.NODE_ENV},on port : ${PORT}`.yellow.bold
